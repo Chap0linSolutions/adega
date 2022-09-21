6792f5042ff235a6ead6a95d588245d9e7b70cb5
Carlos Rocha <carlosrocha@mecajun.com>
2022-09-21 18:41:37 -0300

    feat(GAME-11): adicionando eslint

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import cors from 'cors';
   
 const app = express();
+
 app.use(cors())
-const PORT:Number=3000;
+const PORT = 3000;
   
 app.get('/', (req, res) => {
-    res.send('Welcome to typescript backend Guy!');
+    res.send('Bem vindo à adega!');
 })
   
 app.listen(PORT,() => {
