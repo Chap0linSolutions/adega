b15e219befac1a5c377855f46a29ad9a249eaf54
chap0lin <carlosrochacaar@gmail.com>
2022-10-10 23:50:19 -0300

    running prettier

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-  
+
 const app = express();
 
-app.use(cors())
+app.use(cors());
 const PORT = 3000;
-  
+
 app.get('/', (req, res) => {
-    res.send('Bem vindo à adega!');
-})
-  
-app.listen(PORT,() => {
-    console.log('The application is listening '
-          + 'on port http://localhost:'+PORT);
-})
+  res.send('Bem vindo à adega!');
+});
+
+app.listen(PORT, () => {
+  console.log(
+    'The application is listening ' + 'on port http://localhost:' + PORT
+  );
+});
