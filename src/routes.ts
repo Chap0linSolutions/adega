589a5288b1c3dccc4192dd4bd4f1576380a77875
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-10-25 16:08:33 -0300

    Added room code validation

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,16 @@ import express from 'express';
 import ExampleController from './controllers/ExampleController';
 const exampleController = new ExampleController();
 
+import RoomAccessController from './controllers/RoomAccessController';
+const roomAccessController = new RoomAccessController();
+
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
   res.send('Bem vindo à adega!');
 });
+
+routes.get('/roomCode/:code', roomAccessController.joinRoom);
 
 routes.get('/example', exampleController.index);
 
