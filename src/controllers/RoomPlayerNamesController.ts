39c1258dbc32c5985e49a3ed724bd0c9edff9859
AmigoDoCarlos <axelpinheiro@gmail.com>
2022-11-26 21:34:12 -0300

    adicionado impedimento de dois nomes iguais na mesma sala - backend.

---
+++ src/controllers/RoomPlayerNamesController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import Store from '../realtime/store';
+
+export default class RoomPlayerNameController {
+    async checkNameAvailabiliy(req: Request, res: Response) {
+        const room = req.params.roomCode;
+        const nameToVerify = req.params.userName;
+        console.log(`Sala ${room} - verificando se '${nameToVerify}' está disponível... `);
+
+        const currentPlayersInRoom = Store.getInstance().rooms.get(room);
+        const nameInUse = currentPlayersInRoom!.players.find(player => player.nickname === nameToVerify);
+
+        if(nameInUse){
+            console.log('Nome já em uso.');
+            res.status(400).send();
+        } else {
+            console.log('Nome liberado.');
+            res.status(200).send(true);
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,9 @@ const exampleController = new ExampleController();
 import RoomAccessController from './controllers/RoomAccessController';
 const roomAccessController = new RoomAccessController();
 
+import RoomPlayerNameController from './controllers/RoomPlayerNamesController';
+const roomPlayerNameController = new RoomPlayerNameController();
+
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
@@ -16,5 +19,7 @@ routes.get('/roomCode/:code', roomAccessController.joinRoom);
 routes.put('/createRoom', roomAccessController.createRoom);
 
 routes.get('/example', exampleController.index);
+
+routes.get('/nicknameCheck/:roomCode/:userName', roomPlayerNameController.checkNameAvailabiliy);
 
 export default routes;
