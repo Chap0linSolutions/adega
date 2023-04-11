cdb971058b3a8c4c3f11adf430bd7e8008748151
Luiza Moncao <luiza.cmoncao@gmail.com>
2023-04-10 21:52:07 -0300

    [GAME-118] Controllers unit tests implementation

--- src/controllers/RoomPlayerNamesController.ts
+++ src/controllers/RoomPlayerNamesController.ts
@@ -16,10 +16,10 @@ export default class RoomPlayerNameController {
 
       if (nameInUse) {
         console.log('Nome já em uso.');
-        res.status(409).send();
+        res.status(409).send('Nome já em uso.');
       } else {
         console.log('Nome liberado.');
-        res.status(200).send(true);
+        res.status(200).send('Nome liberado.');
       }
     }
   }
