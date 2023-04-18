b3e86dce9481b8c99053dfb29c67cc9b5db090d5
chap0lin <carlosrochacaar@gmail.com>
2023-04-18 18:03:36 -0300

    removing unecessary variables

--- src/controllers/ExampleController.test.ts
+++ src/controllers/ExampleController.test.ts
@@ -4,7 +4,7 @@ import { getMockReq, getMockRes } from '@jest-mock/express'
 test("Testing index method from ExampleController class", async () => {
   const controller = new ExampleController();
   const req = getMockReq();
-  const { res, next } = getMockRes();
+  const { res } = getMockRes();
 
   await controller.index(req, res);
   expect(res.status).toBeCalledWith(200);
