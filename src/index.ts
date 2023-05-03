a4cea818f7c91375acf43b8bad2bd3c153c05224
AmigoDoCarlos <axelpinheiro@gmail.com>
2023-05-03 11:52:42 -0300

    início da compatibilização com o nginx

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,28 @@ import { Server } from 'socket.io';
 import routes from './routes';
 import realtime from './realtime';
 
-const PORT = 3000;
+if(process.argv[2]){
+  const PORT = Number(process.argv[2]);
+  if(Number.isNaN(PORT)){
+    console.log('Porta especificada inválida.');
+    process.exit(0);
+  } else {
+    const app = express();
+    const server = http.createServer(app);
+    app.use(cors());
+    app.use(routes);
 
-const app = express();
-const server = http.createServer(app);
-app.use(cors());
-app.use(routes);
+    const io = new Server(server, { cors: { origin: '*' } });
 
-const io = new Server(server, { cors: { origin: '*' } });
+    realtime(io);
 
-realtime(io);
-
-server.listen(PORT, () => {
-  console.log(
-    'The application is listening ' + 'on port http://localhost:' + PORT
-  );
-});
+    server.listen(PORT, () => {
+      console.log(
+        'A aplicação está rodando em http://localhost:' + PORT
+      );
+    });   
+  }
+} else {
+  console.log('É preciso especificar a porta em que o servidor vai rodar.');
+  process.exit(0);
+}
